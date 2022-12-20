69ed5f267b20c091b4860b2036995ec12a95a42a
faiza <faiza.safeer456@gmail.com>
2022-12-20 13:14:20 +1100

    feautres added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
        CarFeatures XLR=new CarFeatures();
+
         System.out.println("launch the car");
+   XLR.color="Red";
+   XLR.doors="5";
+   XLR.model="a5";
+   
     }
 }
